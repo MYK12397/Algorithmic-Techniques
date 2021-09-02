36b42aec550fba9ff49a74a383f6e9729e63dbc8
MYK12397 <yusufzai.12397@gmail.com>
2021-09-02 11:41:34 +0530

    Added Some Important Problems on Binary Search

---
+++ Binary_Search/next_alphabet.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+char bs(char a[], int n, char k) {
+	char res = '#';
+	int l = 0, r = n - 1;
+	
+	while (l <= r){
+		int m = l + (r - l) / 2;
+		if (a[m] == k)
+			l = m + 1;
+		else if (a[m] < k)
+			l = m + 1;
+		else {
+			res = a[m];
+			r = m - 1;
+		}
+	}
+	return res;
+}
+
+int main()
+{
+	char a[] = {'b', 'c', 'f', 'h'};
+	int n = sizeof(a) / sizeof(a[0]);
+	char k = 'a';
+	cout << bs(a, n, k);
+	return 0;
+}
+
+// arr[] = a b c g h k
+// char k = 'c'
+// output : g 
